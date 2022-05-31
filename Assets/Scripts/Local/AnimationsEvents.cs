e1c591e7ee9c2c47636a40caec92d6e540a0c2f4
G3oX <moremo_89@hotmail.com>
2022-05-31 22:24:43 +0200

    Added MainMenu v1

---
+++ Assets/Scripts/Local/AnimationsEvents.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace local
+{
+    public class AnimationsEvents : MonoBehaviour
+    {
+        public void deactiveObject()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Local/MainMenuManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+namespace local
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        #region Componentes
+        //Input Field
+        [Header("SERVER NAME")]
+        [Space(1f)]
+        [SerializeField] TMP_InputField inputField;
+
+        // Botones
+        [Header("BUTTONS")]
+        [Space(1f)]
+        [SerializeField] Button hostB;
+        [SerializeField] Button joinB;
+        [SerializeField] Button tutorialB;
+        [SerializeField] Button exitGameB;
+        [SerializeField] Button goBackB;
+
+        // CanvasGroups
+        [Header("GROUPS")]
+        [Space(1f)]
+        [SerializeField] CanvasGroup buttonsGroup;
+        [SerializeField] CanvasGroup mainMenuGroup;
+        [SerializeField] CanvasGroup tutorialGroup;
+
+        // Animacion
+        [Header("ANIMATION")]
+        [Space(1f)]
+        [SerializeField] Animator mainMenuAnim;
+        [SerializeField] Animator tutorialMenuAnim;
+        [SerializeField] float delayForDeactivate;
+
+
+        #endregion
+
+        string serverName;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Invoke("activeMainMenu", 1f);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            serverName = inputField.text;
+            Debug.Log(serverName);
+        }
+
+        #region BUTTONS ACTIONS
+
+        public void loadScene(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+        public void exitGame()
+        {
+            Application.Quit();
+        }
+
+        public void activeMenu(GameObject Menu)
+        {
+            Menu.SetActive(true);
+        }
+
+        public void deactiveMenu(Animator anim)
+        {
+            anim.SetTrigger("a_Close");
+        }
+
+        #endregion
+
+        private void activeMainMenu()
+        {
+            mainMenuGroup.gameObject.SetActive(true);
+        }
+    }
+}
+
